d239c6300f90d2ab80f0e77d10ee37374708dde1
msupernaw <matthew.supernaw@noaa.gov>
2014-03-31 12:53:41 -0400

    Update ADNumber.hpp
    restored original header

--- ADNumber.hpp
+++ ADNumber.hpp
@@ -1,9 +1,61 @@
-/* 
- * File:   ADNumber.hpp
- * Author: matthewsupernaw
- *
- * Created on March 20, 2014, 12:03 PM
+
+/*!
+ *  Software to compute derivatives. 
  */
+
+/*!
+ *   This library is dual-licensed: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version <N> as 
+ *   published by the Free Software Foundation. For the terms of this 
+ *   license, see licenses/gpl_v3.txt or <http://www.gnu.org/licenses/>.
+ *
+ *   You are free to use this library under the terms of the GNU General
+ *   Public License, but WITHOUT ANY WARRANTY; without even the implied 
+ *   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU General Public License for more details.
+ *
+ *  Alternatively, you can license this library under a commercial
+ *  license.
+ *
+ *               ADNumber Commercial License(ACL)
+ *               ================================
+ * ---------------------------------------------------------------------
+ * 
+ *  The license is non-exclusively granted to a single person or company,
+ *  per payment of the license fee, for the lifetime of that person or
+ *  company. The license is non-transferrable.
+ * 
+ *  The ACL grants the licensee the right to use ADNumber in commercial
+ *  closed-source projects. Modifications may be made to ADNumber with no
+ *  obligation to release the modified source code. ADNumber (or pieces
+ *  thereof) may be included in any number of projects authored (in whole
+ *  or in part) by the licensee.
+ * 
+ *  The licensee may use any version of ADNumber, past, present or future,
+ *  as is most convenient. This license does not entitle the licensee to
+ *  receive any technical support, updates or bug fixes, except as such are
+ *  made publicly available to all ADNumber users.
+ * 
+ *  The licensee may not sub-license ADNumber itself, meaning that any
+ *  commercially released product containing all or parts of ADNumber must
+ *  have added functionality beyond what is available in ADNumber;
+ *  ADNumber itself may not be re-licensed by the licensee.
+ * 
+ *  To obtain a commercial license agreement, contact:
+ * 
+ *  Matthew Supernaw
+ *  msupernaw@gmail.com
+ * 
+ */
+
+/*!
+ *
+ * File:   ADNumber.hpp
+ * Author: Matthew R. Supernaw
+ *
+ * Created on January 4, 2012, 7:08 PM
+ */
+
 
 #ifndef ADNUMBER_HPP
 #define	ADNUMBER_HPP
